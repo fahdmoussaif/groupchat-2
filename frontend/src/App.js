900fa8eb1fdd14ce0af550ccd378157b608e707c
fahdmoussaif <moussaif.fahd@proton.me>
2024-12-24 21:12:15 +0100

    chakra ui added

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Welcome to Chakra UI test
+          </h1>
+         <Button>Click me for test</Button>
+        </header>
+      </div>
+  );
+}
+
+export default App;
