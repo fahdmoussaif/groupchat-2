7c7622996920781e97d92213c54ef4eb10a88250
fahdmoussaif <moussaif.fahd@proton.me>
2024-12-24 23:45:14 +0100

    Update Homepage.js

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Button } from "@chakra-ui/react";
 
-const Homepage = () => {
-  return (
-    <div>
-      <h1>Welcome to the homepage!</h1>
-      <Button colorScheme="blue" size="lg">
-        Click me!
-      </Button>
-    </div>
+function Homepage() {
+    return (
+        <div>
+            <p>Homepage</p>
+        </div>
 
-  );
+    );
 }
 
 export default Homepage;
