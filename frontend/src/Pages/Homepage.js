d38a260f3d5f2e6b5a01dc3baf2fd9f64f3901ab
fahdmoussaif <moussaif.fahd@proton.me>
2024-12-25 00:22:32 +0100

    changed homepage

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,12 +1,23 @@
+// src/pages/HomePage.js
 import React from 'react';
+import { Box, Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-function Homepage() {
-    return (
-        <div>
-            <p>Homepage</p>
-        </div>
+const HomePage = () => {
+  return (
+    <Box textAlign="center" p={10}>
+      <Heading>Welcome to Group Messaging App</Heading>
+      <Text mt={4}>Connect and chat with your friends in real-time.</Text>
+      <VStack spacing={4} mt={8}>
+        <Link to="/login">
+          <Button colorScheme="teal">Login</Button>
+        </Link>
+        <Link to="/register">
+          <Button colorScheme="blue">Register</Button>
+        </Link>
+      </VStack>
+    </Box>
+  );
+};
 
-    );
-}
-
-export default Homepage;
+export default HomePage;
