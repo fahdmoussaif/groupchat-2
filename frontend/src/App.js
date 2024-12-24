8777d7943da5b2bfd4578ec65263e7c59026d493
fahdmoussaif <moussaif.fahd@proton.me>
2024-12-25 00:21:59 +0100

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
-import Test from './Pages/test';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="home" element={<Homepage/>} />
-        <Route path='test' element={<Test/>} />
       </Routes>
     </div>
 
