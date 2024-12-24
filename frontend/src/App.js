b99eb77d0772758275c3c8ec0254fae859f2b5a5
fahdmoussaif <moussaif.fahd@proton.me>
2024-12-24 23:45:02 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
+import Test from './Pages/test';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="home" element={<Homepage />} />
+        <Route path="home" element={<Homepage/>} />
+        <Route path='test' element={<Test/>} />
       </Routes>
     </div>
 
