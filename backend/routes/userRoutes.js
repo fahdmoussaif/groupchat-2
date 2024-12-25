1398472a7dda0de47e931ef8a0061c15750533e0
fahdmoussaif <moussaif.fahd@proton.me>
2024-12-25 02:21:57 +0100

    upload

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Register a new user
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const nodemon = require('nodemon');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 5000
+require('dotenv').config();
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/users', require('./routes/userRoutes'));
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
 
